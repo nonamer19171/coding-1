6782f0184391ca92df610cce60bbaea3bf9ccb43
nonamer19171 <mjames938@boyslatin.net>
2025-02-04 19:25:01 +0000

    i made up history work today

--- Unit 1/introPhython.py
+++ Unit 1/introPhython.py
@@ -1,2 +1,17 @@
 message = 'hello from python.'
 'words'
+#the hashtag symbol is a way to make 
+#anything invisible to a computar in python
+
+# text behind the hashtag is great for
+
+#oatatypes
+
+# string detatype 
+# a string is anything wrapped in quotes 
+
+# we create strings by first clicking quote bottom of the keyboard and then typing within the text 
+
+#strings can contain any key on a keyboard 
+
+#strings are use stictly by text data like, messages, comments
